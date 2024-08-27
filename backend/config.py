746fb6e7842c98335cb3c2509dc992897ec83250
Siddharth Asthana <siddharthasthana31@gmail.com>
2024-08-27 17:29:53 +0530

    Minor fix
    Signed-off-by: Siddharth Asthana <siddharthasthana31@gmail.com>

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@db:5432/dataflow')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:mysecretpassword@db:5432/dataflow")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
