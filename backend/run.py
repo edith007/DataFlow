43538c73482ac42065c61eb39e1af74cbc524efe
priyanshukumar397 <54209223+priyanshukumar397@users.noreply.github.com>
2024-09-15 16:47:37 +0530

    Update run.py
    Debug Mode Enabled

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,4 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False) // Running the application in debug mode (debug flag is set to True in run) is a security risk if the application is accessible by untrusted parties.
